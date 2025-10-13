9acf3ed54fa65c5ff408c5a15f399a3abb7717c9
angelo <mjadeveyra@tip.edu.ph>
2025-10-13 23:14:42 +0800

    gelo 2nd push

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import RedeemPage from '../../components/RedeemPage';
 
+
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
